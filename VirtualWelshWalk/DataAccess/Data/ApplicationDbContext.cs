1ab454a222c47b204c107daf13645735fa57feda
zaib97 <inam1_16@uni.worc.ac.uk>
2020-12-15 13:03:39 +0000

    New
    New Login and register work now

---
+++ VirtualWelshWalk/DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VirtualWelshWalk.DataAccess.Models;
+
+namespace VirtualWelshWalk.DataAccess.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<User>
+    {
+
+        public DbSet<People> PeoplesTBL { get; set; }
+
+        public DbSet<VirtualWalk> VirtualWalksTBL { get; set; }
+
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            // Ignore these columns
+            builder.Entity<User>().
+                Ignore(u => u.PhoneNumber)
+                .Ignore(u => u.PhoneNumberConfirmed);
+        }
+    }
+}
---
+++ VirtualWelshWalk/DataAccess/Models/People.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VirtualWelshWalk.DataAccess.Models
+{
+    /// <summary>
+    /// This class is the db table to people
+    /// Containing ID first name, last name and a foreign key
+    /// </summary>
+    public class People
+    {
+        [Key]
+        public int PeopleId { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string LastName { get; set; }
+
+        public List<VirtualWalk> VirtualWalk { get; set; }
+    }
+}
