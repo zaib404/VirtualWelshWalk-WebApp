389f3808cc72dab432184e57d4df493f5841260b
zaib97 <inam1_16@uni.worc.ac.uk>
2021-01-09 22:31:10 +0000

    index page html css kinda done

--- VirtualWelshWalk/obj/Debug/net5.0/RazorDeclaration/Pages/Index.razor.g.cs
+++ VirtualWelshWalk/obj/Debug/net5.0/RazorDeclaration/Pages/Index.razor.g.cs
@@ -98,33 +98,34 @@ using VirtualWelshWalk.Shared;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 98 "D:\Zaib\Documents\Areca Design\VirtualWelshWalk\VirtualWelshWalk\Pages\Index.razor"
-       
+#line 266 "D:\Zaib\Documents\Areca Design\VirtualWelshWalk\VirtualWelshWalk\Pages\Index.razor"
+           
 
-    private string Welcome = "Welcome to Educ8’s Welsh Cultural Coastal Walk";
-    //IJSObjectReference jsSlide;
+        private string Welcome = "Welcome to Educ8’s Welsh Cultural Coastal Walk";
+        //IJSObjectReference jsSlide;
 
-    ////protected override async Task OnAfterRenderAsync(bool firstRender)
-    ////{
-    ////    if (firstRender)
-    ////    {
-    ////        jsSlide = await jsRunTime.InvokeAsync<IJSObjectReference>(
-    ////        "import", "./scripts/Slides.js");
-    ////    }
+        ////protected override async Task OnAfterRenderAsync(bool firstRender)
+        ////{
+        ////    if (firstRender)
+        ////    {
+        ////        jsSlide = await jsRunTime.InvokeAsync<IJSObjectReference>(
+        ////        "import", "./scripts/Slides.js");
+        ////    }
 
-    ////    if (jsSlide != null)
-    ////    {
-    ////        await jsSlide.InvokeVoidAsync("showSlides");
-    ////    }
-    ////}
+        ////    if (jsSlide != null)
+        ////    {
+        ////        await jsSlide.InvokeVoidAsync("showSlides");
+        ////    }
+        ////}
 
-    //async ValueTask IAsyncDisposable.DisposeAsync()
-    //{
-    //    if (jsSlide != null)
-    //    {
-    //        await jsSlide.DisposeAsync();
-    //    }
-    //}
+        //async ValueTask IAsyncDisposable.DisposeAsync()
+        //{
+        //    if (jsSlide != null)
+        //    {
+        //        await jsSlide.DisposeAsync();
+        //    }
+        //}
+    
 
 #line default
 #line hidden
--- VirtualWelshWalk/obj/Debug/net5.0/RazorDeclaration/Shared/Footer.razor.g.cs
+++ VirtualWelshWalk/obj/Debug/net5.0/RazorDeclaration/Shared/Footer.razor.g.cs
@@ -82,28 +82,13 @@ using VirtualWelshWalk.Shared;
 #line default
 #line hidden
 #nullable disable
-    public partial class NavMenu : Microsoft.AspNetCore.Components.ComponentBase
+    public partial class Footer : Microsoft.AspNetCore.Components.ComponentBase
     {
         #pragma warning disable 1998
         protected override void BuildRenderTree(Microsoft.AspNetCore.Components.Rendering.RenderTreeBuilder __builder)
         {
         }
         #pragma warning restore 1998
-#nullable restore
-#line 32 "D:\Zaib\Documents\Areca Design\VirtualWelshWalk\VirtualWelshWalk\Shared\NavMenu.razor"
-       
-    private bool collapseNavMenu = true;
-
-    private string NavMenuCssClass => collapseNavMenu ? "collapse" : null;
-
-    private void ToggleNavMenu()
-    {
-        collapseNavMenu = !collapseNavMenu;
-    }
-
-#line default
-#line hidden
-#nullable disable
     }
 }
 #pragma warning restore 1591
